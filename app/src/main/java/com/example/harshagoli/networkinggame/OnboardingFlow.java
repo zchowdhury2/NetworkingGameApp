a46fbb5cd0c833cd19560c3d9aa25c901812c77c
Harsha Goli <harshagoli@gmail.com>
2018-11-18 20:24:32 -0500

    removed random words

--- app/src/main/java/com/example/harshagoli/networkinggame/OnboardingFlow.java
+++ app/src/main/java/com/example/harshagoli/networkinggame/OnboardingFlow.java
@@ -111,8 +111,8 @@ public class OnboardingFlow extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_onboarding_flow, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
